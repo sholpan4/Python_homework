f5763a47668cc574bab325bfa391e3a8db1a4a05
sholpan4 <b.o.sholpan44@gmail.com>
2023-05-27 12:57:42 +0600

    may19
    may19

--- May19/task9.py
+++ May19/task9.py
@@ -3,7 +3,7 @@ user_in = input("Enter 5 digital number:")
 num = int(user_in)
 
 last_digit = num % 10
-num = num // 10 #деление без остатка
+num = num // 10 
 new_number = last_digit * 10000 + num
 
 print(new_number)
