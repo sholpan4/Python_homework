833d143112ded67111a291eb47775e2d2d05e751
vitnemikin <49492880+vitnemikin@users.noreply.github.com>
2023-05-28 20:21:42 +0600

    Проверено дз от 23.05
    Комментарии к коду

--- May23/task5.py
+++ May23/task5.py
@@ -2,7 +2,8 @@
 user_in = input("Enter word:")
 my_symbol1 = user_in[0]
 my_symbol2 = user_in[-1]
-first_slice = user_in[1:-2]
+first_slice = user_in[1:-2] # VN: здесь неправильный второй индекс. Из-за этого "торт" превращается в "т*т"
+                            # лучше всего было бы вычислить filler_length = len(user_in) - 2
 
 len(first_slice)
 filler = "*"
