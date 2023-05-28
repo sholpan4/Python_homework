dceb8e9824cc145dbcee41e557c949aef5b4d158
vitnemikin <49492880+vitnemikin@users.noreply.github.com>
2023-05-28 20:05:58 +0600

    Проверено дз от 19.05
    Комментарий

--- May19/task2.py
+++ May19/task2.py
@@ -5,5 +5,5 @@ user_int1 = int(user_input)
 user_input = input("Enter number: ")
 user_int2 = int(user_input)
 
-result = (user_int1+user_int2) // 2
+result = (user_int1+user_int2) // 2  # VN: Среднее арифметическое может быть дробным
 print("Result is", result)
