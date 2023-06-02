b8cb66c18bab655ae01877d3822da496f824d73a
sholpan4 <b.o.sholpan44@gmail.com>
2023-06-02 12:11:44 +0600

    May30
    May30

--- May30/task3.py
+++ May30/task3.py
@@ -3,8 +3,10 @@ user_in = input("Enter a year:")
 try:
     year = int(user_in)
 except ValueError:
-    message = "Please enter a year in 4 digit numbers!"
+    message = "Please enter a year in numbers!"
 except TypeError:
+    message = "You've entered not a number!"
+except NameError:
     message = "You've entered not a number!"
 
 if (year % 4 == 0) and (year % 100 != 0) or (year % 400 == 0):
