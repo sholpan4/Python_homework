202cca8037f114a9c771019953aecfa3443e3df7
sholpan4 <b.o.sholpan44@gmail.com>
2023-06-04 02:04:04 +0600

    June2h
    June2h

--- Draft/draft.py
+++ Draft/draft.py
@@ -1,27 +1,27 @@
 user_in = input("Enter the cost of purchase:")
-cost = int(user_in)
-#try:
-    #cost = int(user_in)
-#except ValueError:
-    #cost = ""
-    #message = "Please enter a number!"
-#if type(cost) is int():
-if cost < 200:
-    result = cost + 1
-    template = "There is no discount. Total amount is $ %d" 
-    message = template % result
-elif 200 <= cost < 300:
-    result = cost * 0.03
-    template = "Discount 3%. Total amount is $ %d" 
-    message = template % result
-elif 300 <= cost < 500:
-    result = cost * 0.05
-    template = "Discount 5%. Total amount is $ %d" 
-    message = template % result
-elif cost >= 500: 
-    result = cost * 0.05
-    template = "Discount 7%. Total amount is $ %d" 
-    message = template % result
+#cost = int(user_in)
+try:
+    cost = int(user_in)
+except ValueError:
+    cost = ""
+    message = "Please enter a number!"
+if type(cost) is int:
+    if cost < 200:
+        result = cost
+        template = "There is no discount. Total amount is $ %d" 
+        message = template % result
+    elif 200 <= cost < 300:
+        result = cost * 0.03
+        template = "Discount 3%. Total amount is $ %d" 
+        message = template % result
+    elif 300 <= cost < 500:
+        result = cost * 0.05
+        template = "Discount 5%. Total amount is $ %d" 
+        message = template % result
+    else: # cost >= 500: 
+        result = cost * 0.05
+        template = "Discount 7%. Total amount is $ %d" 
+        message = template % result
 else:
     message = "Please enter a number!"
     
