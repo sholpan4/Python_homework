9396d68879fd3e953e93a20310f46dcbf03a2ab1
sholpan4 <b.o.sholpan44@gmail.com>
2023-06-09 17:34:07 +0600

    june2h
    june2h

--- June2/task5.py
+++ June2/task5.py
@@ -2,8 +2,8 @@ def get_volume(height, radian):
     pi = 3.14159
     return pi * radian ** 2 * height
 
-height = float(input('Height of cylinder: '))
-radian = float(input('Radius of cylinder: '))
+height = float(input('Height of cylinder in meter: '))
+radian = float(input('Radius of cylinder in meter: '))
 
 volume = float(get_volume)
 print("Volume is: ", volume)
