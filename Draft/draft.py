e508f0003808d73901131026a56e20d72a057b89
sholpan4 <b.o.sholpan44@gmail.com>
2023-06-11 20:17:45 +0600

    june2h
    june2h

---
+++ Draft/draft.py
@@ -0,0 +1,14 @@
+def recur(num):
+    
+    if num < 0:  
+        return
+
+    recur(num-1)
+    print(num)
+
+recur(4)
+
+def get_sum_recur(recur):
+    sum = recur_result
+    
+get_sum_recur(recur)
