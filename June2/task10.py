59622fa8ad9d6556876f3cb55e951f71f550ff80
sholpan4 <b.o.sholpan44@gmail.com>
2023-06-11 14:57:52 +0600

    june2h
    june2h

---
+++ June2/task10.py
@@ -0,0 +1,12 @@
+
+def check_perfect_number(num):
+    mylist = [2, 3, 5, 7, 13, 17, 19, 89, 107, 127]   
+    for p in mylist:
+        if 2 ** (p - 1) * (2 ** p -1) == num:
+            print("%d is a perfect number!" % num)
+            break
+    else:
+        print("This is not a perfect number!")
+        
+num = int(input("Enter the number:"))
+check_perfect_number(num)
--- June2/task9.py
+++ June2/task9.py
@@ -1,4 +1,12 @@
-
-
-p [2, 3, 5, 7, 13, 17, 19, 89, 107, 127]
-2 ** (p - 1) * (2 ** p -1)
+#task9
+def check_perfect_number(num):
+    mylist = [2, 3, 5, 7, 13, 17, 19, 89, 107, 127]   
+    for p in mylist:
+        if 2 ** (p - 1) * (2 ** p -1) == num:
+            print("%d is a perfect number!" % num)
+            break
+    else:
+        print("This is not a perfect number!")
+        
+num = int(input("Enter the number:"))
+check_perfect_number(num)
