2f11f936c2762cc8ee6df715bd9cbd5a2e7ccabc
sholpan4 <b.o.sholpan44@gmail.com>
2023-06-11 19:21:33 +0600

    june2h
    june2h

--- June2/task10.py
+++ June2/task10.py
@@ -1,12 +1,12 @@
-
-def check_perfect_number(num):
-    mylist = [2, 3, 5, 7, 13, 17, 19, 89, 107, 127]   
-    for p in mylist:
-        if 2 ** (p - 1) * (2 ** p -1) == num:
-            print("%d is a perfect number!" % num)
-            break
-    else:
-        print("This is not a perfect number!")
+#task10
+def check_perfect_number(num, num2):
+    for i in range(num, num2):    
+        mylist = [2, 3, 5, 7, 13, 17, 19, 89, 107, 127]   
+        for p in mylist:
+            if 2 ** (p - 1) * (2 ** p -1) == i:
+                print("%d is a perfect number!" % i)
+                break           
         
-num = int(input("Enter the number:"))
-check_perfect_number(num)
+num = int(input("Enter the minumum number:"))
+num2 = int(input("Enter the maximum number:"))
+check_perfect_number(num, num2)
--- June2/task2.py
+++ June2/task2.py
@@ -1,8 +1,8 @@
+
 def get_sum(n):
   if n > 0:  
     result = 0 + (n - 1)
-    return result
-    
+    return result    
 
 n = get_sum (5)
 print(n)
