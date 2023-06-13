e3df6510331ee3b2f8efd215b347821d1a19bb16
sholpan4 <b.o.sholpan44@gmail.com>
2023-06-13 17:49:21 +0600

    draft
    draft

--- Draft/draft2.py
+++ Draft/draft2.py
@@ -1,7 +1,7 @@
 list_hor = ["a", "b", "c", "d", "e", "f", "g", "h"]
-list_ver = [2,3,4,5,6,7,8,9,]
+list_ver = [1,2,3,4,5,6,7,8]
 
-for b in list_ver:
-    for a in list_hor:
-        result = 
-        print("%d * %d = " % (b, a), result)
+for a in list_hor:
+    for b in list_ver:
+        #result = 
+        print(a, b)
