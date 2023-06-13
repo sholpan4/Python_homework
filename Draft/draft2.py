9e036ab714896ae28c3309e44d9cb928994faf97
sholpan4 <b.o.sholpan44@gmail.com>
2023-06-13 17:35:09 +0600

    junedraft
    junedraft

--- Draft/draft2.py
+++ Draft/draft2.py
@@ -1,6 +1,7 @@
-def factor(num):
-    if num ==1:
-        return 1
-    return num * factor(num-1)
-result = factor(6)
-print(result)
+list_hor = ["a", "b", "c", "d", "e", "f", "g", "h"]
+list_ver = [2,3,4,5,6,7,8,9,]
+
+for b in list_ver:
+    for a in list_hor:
+        result = 
+        print("%d * %d = " % (b, a), result)
