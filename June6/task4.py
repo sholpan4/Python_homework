292ff8c55b0fa22aa4aeca6d89952dca4d6772e8
sholpan4 <b.o.sholpan44@gmail.com>
2023-06-15 17:53:55 +0600

    june6
    june6

--- June6/task4.py
+++ June6/task4.py
@@ -11,30 +11,29 @@ print("Let's play game. You have to guess the number with 7 attempts. Good luck!
 
 
 def game():
-    counter = 7
-    user_in = input("Your number from 0 tll 100:")
-    #counter = 7
     
+    user_in = input("Your number from 0 tll 100:")
+    counter = 7
+   
     try:
         num = int(user_in)
     except ValueError:
         print("Enter only number!")
         game()
+    
+    if my_random > num:
+        counter -= 1
+        print("Take bigger. You have %d attempts left." % counter)
+        game()
+    elif my_random < num:
+        counter -= 1
+        print("Take smaller. You have %d attempts left." % counter)
+        game()
+    elif counter == 0:
+        print("You lose! The number was %d" % random)
     else:
-        while my_random > num:
-            count = counter - 1
-            print("Take bigger. You have %d attempts left." % count)
-            game()
-        while my_random < num:
-            count = counter - 1
-            print("Take smaller. You have %d attempts left." % count)
-            game()
-        while counter == 0:
-            print("You lose! The number was %d" % random)
-        else:
-
-            print("Congratulation! You have won with %d attemts. The number is %d" % (counter, my_random))
-            return
+        print("Congratulation! You have won with %d attemts. The number is %d" % (counter, my_random))
+        return
 
 my_random = get_random_int (0, 100)
 game()
