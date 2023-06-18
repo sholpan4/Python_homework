89c1c95291d1f05cd623a2eb3ac497855c127a58
sholpan4 <b.o.sholpan44@gmail.com>
2023-06-19 01:08:36 +0600

    hw
    hw

--- June6/task4.py
+++ June6/task4.py
@@ -9,32 +9,29 @@ def get_random_int(min, max):
 
 print("Let's play game. You have to guess the number with 7 attempts. Good luck!")
 
-
+i = 0
 def game():
-    
     user_in = input("Your number from 0 tll 100:")
-    counter = 7
-   
+
     try:
         num = int(user_in)
     except ValueError:
         print("Enter only number!")
         game()
-    
+
     if my_random > num:
-        counter -= 1
-        print("Take bigger. You have %d attempts left." % counter)
+        print("Take bigger.") 
         game()
     elif my_random < num:
-        counter -= 1
-        print("Take smaller. You have %d attempts left." % counter)
+        print("Take smaller. ")    
         game()
-    elif counter == 0:
-        
-        print("You lose! The number was %d" % random)
     else:
-        print("Congratulation! You have won with %d attemts. The number is %d" % (counter, my_random))
-        return
+        print("Congratulation! You have won. The number is %d" % my_random)
+        
+
+   
+    i += 1
+    print(i, "attemts")
 
 my_random = get_random_int (0, 100)
 game()
