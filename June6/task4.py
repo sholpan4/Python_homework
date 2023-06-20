5f16266ddf0fcc34aa89556ca994131d8f582004
sholpan4 <b.o.sholpan44@gmail.com>
2023-06-20 13:33:20 +0600

    june6
    june6

--- June6/task4.py
+++ June6/task4.py
@@ -1,37 +1,35 @@
 
 import random
 
-def get_random_int(min, max):
-    
+def get_random_int(min, max):    
     result = random.random() * (max - min) + min
-
     return int(result)
 
 print("Let's play game. You have to guess the number with 7 attempts. Good luck!")
 
-i = 0
-def game(num, attemts = 7):
-    num = input("Your number from 0 tll 100:")
-
+def game(): 
+    user_in = input("Your number from 0 tll 100:")
+   
     try:
-        num = int(num)
+        num = int(user_in)
     except ValueError:
         print("Enter only number!")
-        game(num)
-
+        game()
+    
     if my_random > num:
-        attemts -= 1
-        print("Take bigger. %d attemts" % attemts) 
-        game(num)
-    elif my_random < num:
-        attemts -= 1
-        print("Take smaller. %d attemts" % attemts)    
-        game(num)
+        print("Take bigger.")
+        game()
+    elif my_random < num:       
+        print("Take smaller.")
+        game()
+    
     else:
-        attemts -= 1
-        print("Congratulation! You have won. The number is %d." % my_random)
-    if attemts == 0:
-        print("You lose!")
+        print("Congratulation! The random number was %d" % my_random)
+        return
 
 my_random = get_random_int (0, 100)
 game()
+
+answer = input("Would you like to play once more?")
+if answer == "yes" or answer == "да":
+    game()
--- June6/task5.py
+++ June6/task5.py
@@ -1,19 +1,14 @@
 
 import random
 
-def get_random_int(min, max):
-    
+def get_random_int(min, max):    
     result = random.random() * (max - min) + min
-
     return int(result)
 
 print("Let's play game. You have to guess the number with 7 attempts. Good luck!")
 
-
 def game():
-    
     user_in = input("Your number from 0 tll 100:")
-   
    
     try:
         num = int(user_in)
