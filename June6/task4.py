e7b31c6cee70fd5e41c241714d10ab651b80c353
sholpan4 <b.o.sholpan44@gmail.com>
2023-06-24 13:43:51 +0600

    june6
    june6

--- June6/task4.py
+++ June6/task4.py
@@ -1,4 +1,4 @@
-
+#task4
 import random
 def get_random_int(min, max):
     result = random.random() * (max - min) + min
@@ -6,30 +6,36 @@ def get_random_int(min, max):
 
 print("Let's play game. You have to guess the number with 7 attempts. Good luck!")
 
-def game(): 
+
+def game(get_random_int, counter): 
     user_in = input("Your number from 0 tll 100:")
-    counter = 7
+    
     try:
         num = int(user_in)
     except ValueError:
         print("Enter only number!")
-        game()
+        game(get_random_int, counter)
 
-
+    if counter == 0:
+        print("You lose! You have use all 7 attempts.")
+        return
+        
     if my_random > num:
-        print("Take bigger.")
-        game()
-    elif my_random < num:       
-        print("Take smaller.")
-        game()
+        counter -= 1 != 0
+        print("Take bigger. Left %d attempts." % counter)
+        game(get_random_int, counter)
+    elif my_random < num: 
+        counter -= 1 != 0   
+        print("Take smaller. Left %d attempts." % counter)
+        game(get_random_int, counter)
     
     else:
-        print("Congratulation! The random number was %d" % my_random)
+        print("Congratulation! The random number was %d." % my_random)
         return
 
 my_random = get_random_int (0, 100)
-game()
+game(get_random_int, 7)
 
 answer = input("Would you like to play once more?")
 if answer == "yes" or answer == "да":
-    game()
+    game(get_random_int, 7)
