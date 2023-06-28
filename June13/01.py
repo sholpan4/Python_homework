0716b543fc746cb69b2fd3c11dfd71814c90c635
sholpan4 <b.o.sholpan44@gmail.com>
2023-06-28 13:16:28 +0600

    13
    13

--- June13/01.py
+++ June13/01.py
@@ -11,20 +11,28 @@
 class Earphone:
     color = " "
     model = " "
-    size = 0
+    weight = 0
     volume = 0
     bottom = True or False
     charge = 0
-    
-    
+        
+    def __init__(self, color, model, weight):
+        self.color = color
+        self.model = model
+        self.weight = weight
+        
+    def __repr__(self) -> str:
+        msg = "Earphone color: %s, model: %s, weight: %d gramm." % (self.color, self.model, self.weight)
+        return msg    
+            
     def play_music(self, bottom):
         bottom = input("Play? ")
-        if bottom == "yes" or True:
+        if bottom == "yes":
             print("Playing the music.")
             
     def stop_music(self, bottom):
-        bottom = input("Stop?")
-        if bottom == "yes" or False:
+        bottom = input("If you want to stop music press 'no': ")
+        if bottom == "no" or False:
             print("Music is stopped.")
             
     def volume_up(self, volume):
@@ -40,14 +48,21 @@ class Earphone:
             print("Can't rise volume up!")
             
     def charge(self, charge):
-        charge = input("Charge? Yes/No")
-        if answer == "Yes":
-            print("Charged")
+        charge = int(input("Would like to charge till 100% press '100': "))
+        if charge == 100:          
+            print("Charged 100%")
         else:
-            print("Battery is low!")
+            print("Battery is low! 0%")
             
-earphone_1 = Earphone()
-earphone_1.volume_up(70)
-print(earphone_1)
+    print("This earphone is ", __name__)
+            
+earphone_1 = Earphone("black", "Marshall", 461)
+#earphone_2 = Earphone("silver", "AirPods Max", 384)
 
-earphone
+earphone_1.volume_up(0)
+#earphone_1.play_music("")
+#earphone_2.stop_music("")
+#earphone_2.charge(0)
+
+print(earphone_1)
+#print(earphone_2)
--- June13/02.py
+++ June13/02.py
@@ -1,2 +1,16 @@
 class Calculator:
-    
+    color = " "
+    model = " "
+    weight = 0
+    volume = 0
+    bottom = True or False
+    charge = 0
+        
+    def __init__(self, color, model, weight):
+        self.color = color
+        self.model = model
+        self.weight = weight
+        
+    def __repr__(self) -> str:
+        msg = "Earphone color: %s, model: %s, weight: %d gramm." % (self.color, self.model, self.weight)
+        return msg 
