1a4d321089569b7f406ea17cd1ce1dae3bf92d92
sholpan4 <b.o.sholpan44@gmail.com>
2023-07-04 16:17:13 +0600

    30
    30

---
+++ June30/01.py
@@ -0,0 +1,6 @@
+tongue_twisters = "Can you can a can as a canner can can a can?"
+
+#задача: вычеслит сколько раз повторяется слово 'can' и вернуть текст с сделав заглавними все первые буквы 
+
+print(tongue_twisters.count("can"))
+print(tongue_twisters.title())
---
+++ June30/02.py
@@ -0,0 +1,5 @@
+tongue_twisters = "She saw", "sharif's", "shoesonthesofa.   ", "But was she so sure those were Sharif's shoes she saw?"
+
+# Преобразовать заданный текст в правильный формат.
+# Правильный формат:
+# She saw Sharif's shoes on the sofa. But was she so sure those were Sharif's shoes she saw?
