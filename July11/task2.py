1b1485efa8d6d47b2337493133a2b4e6aa870eb5
sholpan4 <b.o.sholpan44@gmail.com>
2023-07-18 17:31:40 +0600

    11
    11

--- July11/task2.py
+++ July11/task2.py
@@ -6,16 +6,22 @@
 #     mykey.write(key)
 
 text = 'SOS'
+print(len(text))
+result = [ord(text[0]), ord(text[1]), ord(text[2])]
+print(result)
 
 def get_letter(word):
     secret_txt = ''
-    alph = 'OPQRSTUV'
-    for i in range(len(alph)):
-        if word in alph:
-            secret_txt += alph[i + 1]
-        return secret_txt
+#     # alph = 'OPQRSTUV'
+#     # a = ord(word[0])
+#     # b = ord(word[1])
+#     # c = ord(word[2])
+    for i in word:
+        secret_txt += ord(word[i]+1)
+    return secret_txt
+       
      
-print(text) 
+# # print(text) 
 print(get_letter(text))
-result = list(filter(get_letter, text))   
-print(result)
+# # result = list(filter(get_letter, text))   
+# # print(result)
