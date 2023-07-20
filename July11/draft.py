274dcc48205a3ae152a42361a56c5c8684571f6f
sholpan4 <b.o.sholpan44@gmail.com>
2023-07-20 17:11:25 +0600

    11
    11

--- July11/draft.py
+++ July11/draft.py
@@ -1,10 +1,17 @@
 url= 'https://www.google.com/search?q=palyndromes&rlz=1C1IXYC_ruKZ1059KZ1059&oq=&aqs=chrome.0.69i59i450l8.20045872j0j15&sourceid=chrome&ie=UTF-8'
-# protokol = url.split(':', 1)
-def get_protokol(word):
-    
-    if word.endswith(':'):
-        return word
-    # return word
+protokol_div = url.partition(':')
+protokol = protokol_div[0]
 
-protokol = get_protokol(url)
-print(f'Протокол: "{protokol}"')
+domen_div = url.startswith('www') and url.endswith('com')
+
+# def get_protokol(word):  
+#     if word.endswith(':'):
+#         return word
+#     # return word
+
+# protokol = get_protokol(url)
+print(f"""Протокол: "{protokol}"
+      Домен: "{domen_div}"
+      Путь: ""
+      Параметры запроса: ""
+      """)
