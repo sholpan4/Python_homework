c13652d22c0607fd3bc6c02808da029b6ea5a2f7
sholpan4 <b.o.sholpan44@gmail.com>
2023-08-27 16:11:28 +0600

    PyQt6 calculator H/W
    PyQt6 calculator H/W

---
+++ PyQt6 calculator/main.py
@@ -0,0 +1,4 @@
+
+#python3 -m venv .venv
+#python3 -m pip install PyQt5
+#$Env:QT_QPA_PLATFORM_PLUGIN_PATH = "venv/lib/site-packages/PyQt6/plugins/platforms"
---
+++ PyQt6 calculator/qt.pt.py
@@ -0,0 +1,41 @@
+import sys
+
+from PyQt6.QtWidgets import (
+    QApplication,
+    QGridLayout,
+    QPushButton,
+    QWidget,
+)
+
+app = QApplication([])
+window = QWidget()
+window.setWindowTitle("QGridLayout")
+
+layout = QGridLayout()
+layout.addWidget(QPushButton("AC"), 0, 1)
+layout.addWidget(QPushButton("%"), 0, 3)
+layout.addWidget(QPushButton("/"), 0, 4)
+
+layout.addWidget(QPushButton("7"), 1, 1)
+layout.addWidget(QPushButton("8"), 1, 2)
+layout.addWidget(QPushButton("9"), 1, 3)
+layout.addWidget(QPushButton("*"), 1, 4)
+
+layout.addWidget(QPushButton("4"), 2, 1)
+layout.addWidget(QPushButton("5"), 2, 2)
+layout.addWidget(QPushButton("6"), 2, 3)
+layout.addWidget(QPushButton("-"), 2, 4)
+
+layout.addWidget(QPushButton("3"), 3, 1)
+layout.addWidget(QPushButton("2"), 3, 2)
+layout.addWidget(QPushButton("1"), 3, 3)
+layout.addWidget(QPushButton("+"), 3, 4)
+
+layout.addWidget(QPushButton("0"), 4, 1, 1, 2)
+layout.addWidget(QPushButton(","), 4, 3)
+layout.addWidget(QPushButton("="), 4, 4)
+
+window.setLayout(layout)
+
+window.show()
+sys.exit(app.exec())
