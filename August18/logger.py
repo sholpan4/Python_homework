c978e36ef5b16ac8d3117f318e911ce5dab2352b
sholpan4 <b.o.sholpan44@gmail.com>
2023-08-28 13:50:20 +0600

    18
    18

--- August18/logger.py
+++ August18/logger.py
@@ -5,3 +5,10 @@ class Logger:
     WARNING = 0
     DEBUG = 0
     TRACE = 0
+
+log = Logger(Logger.DEBUG, file='myapp.log')
+
+
+log.w("Это warning сообщение", 1, 2, 3)
+log.t("Это trace сообщение", 4, 5, 6)
+log.e("Это error сообщение", 7, 8, 9, end='\n\n')
