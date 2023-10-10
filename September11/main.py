f79892efada37d371a24248c26b867f0843d05ed
sholpan4 <b.o.sholpan44@gmail.com>
2023-10-10 15:27:51 +0600

    `
    `

--- September11/main.py
+++ September11/main.py
@@ -1,3 +1,12 @@
-from server import *
-from client import *
+import sys
+from PyQt6.QtWidgets import *
+from client import UDP_Client
+from server import UDP_Server
 
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    app.start()
+
+    app.exec()
