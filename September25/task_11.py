2212ec496df24936960ff18c678e54b380da655e
S <52811487+sholpan4@users.noreply.github.com>
2023-10-20 19:33:43 +0600

    Update task_11.py
    '

--- September25/task_11.py
+++ September25/task_11.py
@@ -1,2 +1,13 @@
 #2. Логирование с временем: Создайте функцию-декоратор,
 #которая добавляет временную метку к логам функций.
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+def log_time(func):
+    def wrapper(*args, **kwargs):
+        star_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        elapsed_time = 
