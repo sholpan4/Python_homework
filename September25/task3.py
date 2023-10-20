a5c3244fee705e846a9e33b7f22309e2a76e2886
sholpan4 <b.o.sholpan44@gmail.com>
2023-10-20 16:07:16 +0600

    `
    `

--- September25/task3.py
+++ September25/task3.py
@@ -1,8 +1,10 @@
-match data:
-    case [x, y]:
-        print(f"List {x} and {y}")
-    case (x, y, z):
-        print(f"Tuple {x}, {y}, {z}")
-    case _:
-        print("Unknown")
+def get_data(data):
+    match data:
+        case [x, y]:
+            print(f"List {x} and {y}")
+        case (x, y, z):
+            print(f"Tuple {x}, {y}, {z}")
+        case _:
+            print("Unknown")
 
+print(get_data(("apple", "grape")))  #не реагирует на скобки круглые и прямые. Только по количеству аргументов сортирует на Лист или Тупл. нужен python 3.10 для проверки)
