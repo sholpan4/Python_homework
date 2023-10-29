e6c9b9aee224a9d56fff97acd1dda8fde909fb87
vitnemikin <49492880+vitnemikin@users.noreply.github.com>
2023-10-30 00:11:19 +0600

    Проверено дз от 17.10
    Комментарии к коду

--- October17Iteratior/task1.py
+++ October17Iteratior/task1.py
@@ -4,9 +4,12 @@ import os
 def get_files_in_directory_tree(root_directory):
     for files in os.walk(root_directory):
         for file in files:
+            print(type(file)) #VN: посмотрите, что собой на самом деле
+            print(file)       # представляет file
             yield os.path.join(file)
  
-root_directory = "October17Iteratior/task1.txt" #какой должен быть результат? не работает
+#root_directory = "October17Iteratior/task1.txt" #какой должен быть результат? не работает
+root_directory = "." #VN: нужна ведь директория. Передадим текущую
 for file_path in get_files_in_directory_tree(root_directory):
     print(file_path)
     
--- October17Iteratior/task4.py
+++ October17Iteratior/task4.py
@@ -7,6 +7,7 @@ def process_large_file(file_path):
             total_lines += 1
             numbers = [int(num) for num in line.split()]
             total_sums += sum(numbers)
+            #VN: тут как раз нужен yield, который вернёт статистику по текущей строке
     return total_lines, total_sums
 
 file_path = "October17Iteratior/task4.txt"  # не могу понять какие данные нужно передать. переданный текст не итерирует(
